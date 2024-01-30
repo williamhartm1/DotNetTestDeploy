56b078ce192cf3167d32ce2c355b21a506d2b14b
unknown <williamhartm@hotmail.com>
2024-01-30 12:20:55 +0100

    initial project

---
+++ DeployTest/DeployTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeployTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
